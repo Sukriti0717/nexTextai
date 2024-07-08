f8c86b2dc4f40b1d71138481fce1220027b8f019
Sukriti Sinha <121672302+Sukriti0717@users.noreply.github.com>
2024-07-08 23:19:22 +0530

    Update intelligent_text_completion.py

--- intelligent_text_completion.py
+++ intelligent_text_completion.py
@@ -4,7 +4,7 @@ model_name = 'gpt2'
 tokenizer = GPT2Tokenizer.from_pretrained(model_name)
 model = GPT2LMHeadModel.from_pretrained(model_name)
 
-def generate_text(prompt, max_length=50):
+def complete_text(prompt, max_length=50):
     inputs = tokenizer(prompt, return_tensors='pt')
     outputs = model.generate(inputs.input_ids, max_length=max_length, num_return_sequences=1)
     return tokenizer.decode(outputs[0], skip_special_tokens=True)
